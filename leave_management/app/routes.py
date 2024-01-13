f0e1308503d1509e4bb6d9ebd791ac0552301c67
navpreet <navpreet.khanuja25@gmail.com>
2024-01-14 00:01:58 +0700

    anyone can request a leave date with reason + date

--- leave_management/app/routes.py
+++ leave_management/app/routes.py
@@ -1,11 +1,11 @@
 # app/routes.py
+from datetime import datetime  # Add this import
 from flask import render_template, request, redirect, url_for
 from app import app, db
 from app.models import LeaveRequest
 
 @app.route('/')
 def leave_list():
-    # return 'Xarlos is amazing.'
     leave_requests = LeaveRequest.query.all()
     return render_template('leave_list.html', leave_requests=leave_requests)
 
@@ -13,9 +13,12 @@ def leave_list():
 def request_leave():
     if request.method == 'POST':
         reason = request.form.get('reason')
-        date = request.form.get('date')
+        date_str = request.form.get('date')
 
-        new_leave_request = LeaveRequest(reason=reason, date=date)
+        # Convert the string date to a Python date object
+        date_obj = datetime.strptime(date_str, '%Y-%m-%d').date()
+
+        new_leave_request = LeaveRequest(reason=reason, date=date_obj)
         db.session.add(new_leave_request)
         db.session.commit()
 
