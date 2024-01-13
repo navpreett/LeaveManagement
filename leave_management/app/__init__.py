3e3cd6bdf1bd31d43f2181bd5cc06cdd948f07f9
navpreet <navpreet.khanuja25@gmail.com>
2024-01-13 22:07:09 +0700

    fixed 404 error

--- leave_management/app/__init__.py
+++ leave_management/app/__init__.py
@@ -1,16 +1,9 @@
-# app/__init__.py
 from flask import Flask
 from config import SECRET_KEY, SQLALCHEMY_DATABASE_URI
 from flask_sqlalchemy import SQLAlchemy
 
-# Create the Flask app instance
 app = Flask(__name__)
 app.config['SECRET_KEY'] = SECRET_KEY
 app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 
-# Initialize the SQLAlchemy instance without passing the app instance
-db = SQLAlchemy()
-
-# Initialize the app with the SQLAlchemy instance
-db.init_app(app)
-
+db = SQLAlchemy(app)
--- leave_management/run.py
+++ leave_management/run.py
@@ -1,5 +1,4 @@
-# run.py
-from app import app
+from app.routes import app
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
